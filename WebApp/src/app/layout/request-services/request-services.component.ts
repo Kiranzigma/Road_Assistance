c1d757ce6722f0d51c1bb3666aea40e9b6408fcd
Harrsini sekar <Sekar.h@husky.neu.edu>
2020-04-21 00:48:50 -0400

    Services Tab for vendor

--- WebApp/src/app/layout/request-services/request-services.component.ts
+++ WebApp/src/app/layout/request-services/request-services.component.ts
@@ -7,19 +7,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserServiceService } from 'src/app/shared/user-service.service';
 import { AppServiceService } from 'src/app/app-service.service';
 import { Iuser } from 'src/app/interface/IResponse';
+import { HistoryDetailsElement } from '../history-details/history-details.component';
+import { routerTransition } from 'src/app/shared/router-animations';
 
-export interface HistoryDetailsElement {
+export interface DetailsElement {
   desc: string;
-  position: number;
   estimatedCost: number;
 }
 
-const ELEMENT_DATA: HistoryDetailsElement[] = [];
+const ELEMENT_DATA: DetailsElement[] = [];
 
 @Component({
   selector: 'app-request-services',
   templateUrl: './request-services.component.html',
-  styleUrls: ['./request-services.component.scss']
+  styleUrls: ['./request-services.component.scss'],
+  animations: [routerTransition()]
 })
 
 export class RequestServicesComponent implements OnInit {
@@ -38,19 +40,37 @@ export class RequestServicesComponent implements OnInit {
 
 
   displayedColumns: string[] = ['position', 'desc', 'estimatedCost'];
-  dataSource = new MatTableDataSource<BillingElement>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<DetailsElement>(ELEMENT_DATA);
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(private router: Router, private fb: FormBuilder, private userService: UserServiceService,
     private appservice: AppServiceService) {
     this.arr = this.router.getCurrentNavigation().extras.state.rowData;
-    
     console.log(this.arr); 
+    this.totalCost = this.arr.totalCost;
+    this.dataSource.paginator = this.paginator;
   }
 
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.arr.listOfServices.forEach(element => {
+      element.desc;
+      element.estimatedCost;
+      ELEMENT_DATA.push({desc: element.desc, estimatedCost:element.estimatedCost});
+    });
+  }
+
+  back(){
+    this.router.navigate(['/layout/RequestDetailsComponent'], { state: { rowData:this.arr }});
+  }
+
+  outputemitted(x: string) {
+    if (this.leftBtn === "Back" && x == "left") {
+      this.back();
+      return;
+    }
   }
 
 }
